6876f1c3a7055b872e75c6225fb8c422cd8b3ca8
Jean-François Viaud <jviaud@univ-lr.fr>
2014-03-22 11:47:16 +0100

    Add missing tests to ClosureSystem 	* there is a strange behaviour on some tests Add random method in Context.java 	* Update Context.iuml 	* Add test Update ChangeLog

--- test/lattice/ClosureSystemTest.java
+++ test/lattice/ClosureSystemTest.java
@@ -11,28 +11,73 @@ import org.junit.Test;
  */
 public class ClosureSystemTest {
     /**
-     * Test for the allClosures method.
-     *
-     * @todo implementing test
+     * Test for the allClosures method for Context.
      */
     @Test
-    public void testallClosures() {
-        assertTrue(true);
+    public void testallClosuresCTX() {
+        Context context = new Context();
+        context.addToAttributes("a");
+        context.addToAttributes("b");
+        context.addToAttributes("c");
+        context.addToObservations("1");
+        context.addToObservations("2");
+        context.addToObservations("3");
+        context.addExtentIntent("1", "a");
+        context.addExtentIntent("1", "b");
+        context.addExtentIntent("2", "a");
+        context.addExtentIntent("3", "b");
+        context.addExtentIntent("3", "c");
+        assertEquals(context.allClosures().size(), 6);
     }
     /**
-     * Test for the closedSetLattice method.
-     *
-     * @todo implementing test
+     * Test for the allClosures method for ImplicationSystem.
      */
     @Test
-    public void testclosedSetLattice() {
-        assertTrue(true);
+    public void testallClosuresIS() {
+            ImplicationalSystem is = new ImplicationalSystem();
+            TreeSet<Comparable> elements = new TreeSet<Comparable>();
+            elements.add('a');
+            elements.add('b');
+            elements.add('c');
+            is.addAllElements(elements);
+            Rule r = new Rule();
+            r.addToPremise('a');
+            r.addToConclusion('b');
+            r.addToConclusion('c');
+            is.addRule(r);
+            assertEquals(is.allClosures().size(), 5);
     }
     /**
-     * Test for the getReductibleElements method.
+     * Test for the closedSetLattice method from a context.
+     *
+     * @todo There's something wrong with that test : diagramLattice is always empty ... See ConceptLattice tests
      */
     @Test
-    public void testgetReducibleElements() {
+    public void testclosedSetLatticeCTX() {
+        Context context = Context.random(10, 3, 5);
+        assertEquals(context.closedSetLattice(true).getNodes().size(), ConceptLattice.diagramLattice(context).getNodes().size());
+        assertEquals(context.closedSetLattice(false).getNodes().size(), ConceptLattice.completeLattice(context).getNodes().size());
+        assertEquals(context.closedSetLattice(true).getEdges().size(), ConceptLattice.diagramLattice(context).getEdges().size());
+        assertEquals(context.closedSetLattice(false).getEdges().size(), ConceptLattice.completeLattice(context).getEdges().size());
+    }
+    /**
+     * Test for the closedSetLattice method from an implication system.
+     *
+     * @todo There's something wrong with that test : diagramLattice is always almost empty ... See ConceptLattice tests
+     */
+    @Test
+    public void testclosedSetLatticeIS() {
+        ImplicationalSystem is = ImplicationalSystem.random(7, 4);
+        assertEquals(is.closedSetLattice(true).getNodes().size(), ConceptLattice.diagramLattice(is).getNodes().size());
+        assertEquals(is.closedSetLattice(false).getNodes().size(), ConceptLattice.completeLattice(is).getNodes().size());
+        assertEquals(is.closedSetLattice(true).getEdges().size(), ConceptLattice.diagramLattice(is).getEdges().size());
+        assertEquals(is.closedSetLattice(false).getEdges().size(), ConceptLattice.completeLattice(is).getEdges().size());
+    }
+    /**
+     * Test for the getReductibleElements method for ImplicationalSystem.
+     */
+    @Test
+    public void testgetReducibleElementsIS() {
         ImplicationalSystem is = new ImplicationalSystem();
         TreeSet<Comparable> elements = new TreeSet<Comparable>();
         elements.add(0);
@@ -51,19 +96,61 @@ public class ClosureSystemTest {
         assertTrue(is.getReducibleElements().get(2).isEmpty());
     }
     /**
-     * Test for the nextClosure method.
-     *
-     * @todo implementing test
+     * Test for the getReductibleElements method for Context.
      */
     @Test
-    public void testnextClosure() {
-        assertTrue(true);
+    public void testgetReducibleElementsCTX() {
+        Context context = new Context();
+        context.addToAttributes("a");
+        context.addToAttributes("b");
+        context.addToAttributes("c");
+        context.addToObservations("1");
+        context.addExtentIntent("1", "a");
+        context.addExtentIntent("1", "b");
+        assertEquals(context.getReducibleElements().size(), 1);
     }
     /**
-     * Test for the precedenceGraph method.
+     * Test for the nextClosure method for Context.
      */
     @Test
-    public void testprecedenceGraph() {
+    public void testnextClosureCTX() {
+        Context context = new Context();
+        context.addToAttributes("a");
+        context.addToAttributes("b");
+        context.addToAttributes("c");
+        context.addToObservations("1");
+        context.addToObservations("2");
+        context.addToObservations("3");
+        context.addExtentIntent("1", "a");
+        context.addExtentIntent("1", "b");
+        context.addExtentIntent("2", "a");
+        context.addExtentIntent("3", "b");
+        context.addExtentIntent("3", "c");
+        assertEquals(context.nextClosure(new Concept(context.closure(new ComparableSet()), false)).toString(), "[b]");
+    }
+    /**
+     * Test for the nextClosure method for ImplicationalSystem.
+     */
+    @Test
+    public void testnextClosureIS() {
+        ImplicationalSystem is = new ImplicationalSystem();
+        TreeSet<Comparable> elements = new TreeSet<Comparable>();
+        elements.add('a');
+        elements.add('b');
+        elements.add('c');
+        is.addAllElements(elements);
+        Rule r = new Rule();
+        r.addToPremise('a');
+        r.addToConclusion('b');
+        r.addToConclusion('c');
+        is.addRule(r);
+        assertEquals(is.nextClosure(new Concept(is.closure(new ComparableSet()), false)).toString(), "[c]");
+    }
+    /**
+     * Test for the precedenceGraph method for ImplicationalSystem.
+     */
+    @Test
+    public void testprecedenceGraphIS() {
         ImplicationalSystem is = new ImplicationalSystem();
         TreeSet<Comparable> elements = new TreeSet<Comparable>();
         elements.add(0);
@@ -74,5 +161,25 @@ public class ClosureSystemTest {
         r1.addToConclusion(1);
         is.addRule(r1);
         assertEquals(is.precedenceGraph().getEdges().size(), 1);
+    }
+    /**
+     * Test for the precedenceGraph method for Context.
+     */
+    @Test
+    public void testprecedenceGraphCTX() {
+        Context context = new Context();
+        context.addToAttributes("a");
+        context.addToAttributes("b");
+        context.addToAttributes("c");
+        context.addToObservations("1");
+        context.addToObservations("2");
+        context.addToObservations("3");
+        context.addExtentIntent("1", "a");
+        context.addExtentIntent("1", "b");
+        context.addExtentIntent("2", "a");
+        context.addExtentIntent("3", "b");
+        context.addExtentIntent("3", "c");
+        assertEquals(context.precedenceGraph().getNodes().size(), 3);
+        assertEquals(context.precedenceGraph().getEdges().size(), 1);
     }
 }
