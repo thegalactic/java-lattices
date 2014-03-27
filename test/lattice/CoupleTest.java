3e2debccbc851742a2409615ab9bbe573397569a
Jean-François Viaud <jviaud@univ-lr.fr>
2014-03-27 10:39:26 +0100

    Add LatticeFactory class which generates : 	+ random lattices (by an ugly code) 	+ cartesian product of two lattices 	+ lattice of permutations 	+ boolean algebras
    Consequently add :
    	+ Permutation class for ... lattice of permutations
    	+ Couple class for lattice product
    
    For each of these three classes :
    	+ Add iuml file
    	+ Add unitary tests
    
    Don't forget to update ChangeLog :-)

---
+++ test/lattice/CoupleTest.java
@@ -0,0 +1,82 @@
+package lattice;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ *
+ * @author Jean-François
+ */
+public class CoupleTest {
+    /**
+     * Test Couple constructor.
+     */
+    @Test
+    public void testCouple() {
+        Couple c = new Couple("A", "B");
+        assertEquals(c.getLeft(), "A");
+        assertEquals(c.getRight(), "B");
+    }
+    /**
+     * Test getLeft method.
+     */
+    @Test
+    public void testgetLeft() {
+        Couple c = new Couple("A", "B");
+        assertEquals(c.getLeft(), "A");
+    }
+    /**
+     * Test getRight method.
+     */
+    @Test
+    public void testgetRight() {
+        Couple c = new Couple("A", "B");
+        assertEquals(c.getRight(), "B");
+    }
+    /**
+     * Test setLeft method.
+     */
+    @Test
+    public void testsetLeft() {
+        Couple c = new Couple("A", "B");
+        c.setLeft("C");
+        assertEquals(c.getLeft(), "C");
+    }
+    /**
+     * Test setRight method.
+     */
+    @Test
+    public void testsetRight() {
+        Couple c = new Couple("A", "B");
+        c.setRight("C");
+        assertEquals(c.getRight(), "C");
+    }
+    /**
+     * Test equals method.
+     */
+    @Test
+    public void testequals() {
+        Couple c1 = new Couple("A", "B");
+        Couple c2 = new Couple("A", "B");
+        assertTrue(c1.equals(c2));
+    }
+    /**
+     * Test hashCode method.
+     */
+    @Test
+    public void testhashcode() {
+        Couple c = new Couple("A", "B");
+        Object cc = new Object();
+        cc = (Object) c;
+        assertEquals(c.hashCode(), cc.hashCode());
+    }
+    /**
+     * Test toString method.
+     */
+    public void testtoString() {
+        Couple c = new Couple("A", "B");
+        String str = c.toString();
+        assertEquals(str, "(A,B)");
+    }
+}
