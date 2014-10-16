c932917468cd394f49c2fdd2f8c67326ca062660
Christophe Demko <chdemko@gmail.com>
2014-10-17 00:54:20 +0200

    Deploying

--- src/main/scripts/initiate-publish.sh
+++ src/main/scripts/initiate-publish.sh
@@ -24,7 +24,37 @@ if [ "$TRAVIS_REPO_SLUG" == "kbertet/java-lattices" ] && [ "$TRAVIS_JDK_VERSION"
 	git add -f .
 	git commit -m "    [ci skip] Latest doc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
 	git push -fq origin gh-pages > /dev/null
+
 	echo -e "Published doc to gh-pages.\n"
 
+
+	echo -e "Deploying...\n"
+
+	echo -e "    Getting mvn-repo...\n"
+	cd $HOME
+	git clone --quiet --branch=mvn-repo https://${GH_TOKEN}@github.com/kbertet/java-lattices old-mvn-repo > /dev/null
+	git clone --quiet https://${GH_TOKEN}@github.com/kbertet/java-lattices mvn-repo > /dev/null
+	cd mvn-repo
+	git checkout --orphan mvn-repo
+	git rm -rf .
+	git push -fq origin :mvn-repo > /dev/null
+
+	echo -e "    Deploying locally...\n"
+	cd $current
+	mkdir -p target/mvn-repo/fr/kbertet/lattices
+	test -f $HOME/old-mvn-repo/fr/kbertet/lattices/maven-metadata.xml && cp -f $HOME/old-mvn-repo/fr/kbertet/lattices/maven-metadata.* target/mvn-repo/fr/kbertet/lattices
+	mvn deploy
+
+	cd $HOME/old-mvn-repo
+	cp $current/target/mvn-repo/* .
+	find . -name `basename \`find $current/target/mvn-repo/ -name "*-SNAPSHOT"\`` -exec git rm -rf {} \;
+
+	cd $HOME/mvn-repo
+	cp -Rf $HOME/old-mvn-repo/* .
+	git add -f .
+	git commit -m "    [ci skip] Latest deploy on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to mvn-repo"
+	git push -f origin mvn-repo > /dev/null
+
+	echo -e "Deployed to mvn-repo.\n"
 
 fi
