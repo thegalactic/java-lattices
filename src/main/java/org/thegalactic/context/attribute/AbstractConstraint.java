5c1355d49f644538dc74ef3311f698eb5650d57f
Christophe Demko <chdemko@gmail.com>
2015-03-08 18:50:49 +0100

    Renaming project

--- src/main/java/org/thegalactic/context/attribute/AbstractConstraint.java
+++ src/main/java/org/thegalactic/context/attribute/AbstractConstraint.java
@@ -7,7 +7,7 @@ package org.thegalactic.context.attribute;
  *
  * License: http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html CeCILL-B license
  *
- * This file is part of java-thegalactic.
+ * This file is part of java-lattices.
  * You can redistribute it and/or modify it under the terms of the CeCILL-B license.
  */
 /**
--- src/main/java/org/thegalactic/lattice/BijectiveComponents.java
+++ src/main/java/org/thegalactic/lattice/BijectiveComponents.java
@@ -6,7 +6,7 @@ package org.thegalactic.lattice;
  * Copyright: 2010-2014 Karell Bertet, France
  * License: http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html CeCILL-B license
  *
- * This file is part of java-thegalactic.
+ * This file is part of java-lattices.
  * You can redistribute it and/or modify it under the terms of the CeCILL-B license.
  */
 import java.io.BufferedWriter;
