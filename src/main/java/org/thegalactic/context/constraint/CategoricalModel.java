93f2712ec033e8632bc4300a06639143f734cfa2
Christophe Demko <chdemko@gmail.com>
2016-06-16 21:50:20 +0200

    Adding CategoricalStorage and CategoricalModel classes

---
+++ src/main/java/org/thegalactic/context/constraint/CategoricalModel.java
@@ -0,0 +1,18 @@
+package org.thegalactic.context.constraint;
+
+/*
+ * CategoricalModel.java
+ *
+ * Copyright: 2016 The Galactic Organization, France
+ *
+ * License: http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html CeCILL-B license
+ *
+ * This file is part of java-lattices.
+ * You can redistribute it and/or modify it under the terms of the CeCILL-B license.
+ */
+
+/**
+ * Categorical Model.
+ */
+class CategoricalModel {
+}
---
+++ src/main/java/org/thegalactic/context/constraint/CategoricalStorage.java
@@ -0,0 +1,23 @@
+package org.thegalactic.context.constraint;
+
+/*
+ * CategoricalStorage.java
+ *
+ * Copyright: 2016 The Galactic Organization, France
+ *
+ * License: http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html CeCILL-B license
+ *
+ * This file is part of java-lattices.
+ * You can redistribute it and/or modify it under the terms of the CeCILL-B license.
+ */
+// import java.util.BitSet;
+
+/**
+ * Categorical Storage.
+ */
+public class CategoricalStorage {
+    /**
+     * Model.
+     */
+    private CategoricalModel model;
+}
