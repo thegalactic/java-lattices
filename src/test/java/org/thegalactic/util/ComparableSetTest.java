b8d558d3fbae03b6647a24f90c0fc71fdcd7f76b
Christophe Demko <chdemko@gmail.com>
2016-06-26 17:30:14 +0200

    Fixes unused import

--- src/test/java/org/thegalactic/util/ComparableSetTest.java
+++ src/test/java/org/thegalactic/util/ComparableSetTest.java
@@ -15,7 +15,6 @@ package org.thegalactic.util;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 
 import java.util.TreeSet;
 
