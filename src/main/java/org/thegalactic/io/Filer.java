9a3053d79327675684ea3cce106e14c63db2c884
Christophe Demko <chdemko@gmail.com>
2016-06-26 15:59:30 +0200

    Fixes https://www.codacy.com/app/chdemko/java-lattices_3/file/2842841720/issues/source?bid=3369455&fileBranchId=3369455#l73

--- src/main/java/org/thegalactic/io/Filer.java
+++ src/main/java/org/thegalactic/io/Filer.java
@@ -70,7 +70,7 @@ public final class Filer<E> {
      *
      * @throws IOException When an IOException occurs
      */
-    public void save(final E e, IOFactory factory, final String filename) throws IOException {
+    public void save(final E e, final IOFactory factory, final String filename) throws IOException {
         final BufferedWriter file = new BufferedWriter(new FileWriter(filename));
         factory.getWriter(Filer.getExtension(filename)).write(e, file);
         file.close();
