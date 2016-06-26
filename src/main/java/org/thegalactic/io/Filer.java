2cc2f4097bcd3ba5285cbe384336ec858057c8d4
Christophe Demko <chdemko@gmail.com>
2016-06-26 15:56:10 +0200

    Fixes https://www.codacy.com/app/chdemko/java-lattices_3/file/2842841475/issues/source?bid=3369455&fileBranchId=3369455#l89

--- src/main/java/org/thegalactic/io/Filer.java
+++ src/main/java/org/thegalactic/io/Filer.java
@@ -86,7 +86,7 @@ public final class Filer<E> {
      *
      * @throws IOException When an IOException occurs
      */
-    public void parse(E e, IOFactory factory, final String filename) throws IOException {
+    public void parse(final E e, IOFactory factory, final String filename) throws IOException {
         BufferedReader file = new BufferedReader(new FileReader(filename));
         factory.getReader(Filer.getExtension(filename)).read(e, file);
         file.close();
