1e05ad43c5f21e0d41f9d04bf53972f67dd5cbc4
Jessie Carbonnel <jessie.carbonnel@lirmm.fr>
2017-11-16 10:43:57 +0100

    Testing methods to create a descriptionSet Context and computing all closures

--- src/main/java/org/thegalactic/descriptionset/TestDescriptionSet.java
+++ src/main/java/org/thegalactic/descriptionset/TestDescriptionSet.java
@@ -1,101 +1,58 @@
 package org.thegalactic.descriptionset;
 
-import java.util.TreeSet;
-import java.util.Vector;
-
 public class TestDescriptionSet {
 
 	public static void main(String[] args) {
 		
 		
-//		TreeSet<ComparableDescriptionSet<AttributeDescription>> v = new TreeSet<>();
-//		
-//		AttributeDescription aa = new AttributeDescription("attribute");
-//		aa.init("a b");
-//		
-//		AttributeDescription bb = new AttributeDescription("attribute");
-//		bb.init("b");
-//		
-//		AttributeDescription cc = new AttributeDescription("attribute");
-//		cc.init("b c d");
-//		
-//		AttributeDescription dd = new AttributeDescription("attribute");
-//		dd.init("a b c");
-//		
-//		ComparableDescriptionSet<AttributeDescription> a = ComparableDescriptionSet.create("attribute");
-//		a.add(aa);
-//		
-//		ComparableDescriptionSet<AttributeDescription> b = ComparableDescriptionSet.create("attribute");
-//		b.add(bb);
-//		
-//		ComparableDescriptionSet<AttributeDescription> c = ComparableDescriptionSet.create("attribute");
-//		c.add(cc);
-//		
-//		ComparableDescriptionSet<AttributeDescription> d = ComparableDescriptionSet.create("attribute");
-//		d.add(dd);
-//		
-//		System.out.println(c);
-//		System.out.println(d);
-//		
-//		System.out.println(c.getSimilarity(d));
-//		
-//		v.add(a);
-//		
-//		System.out.println(v);
-//		
-//		v.add(c);
-//		
-//		System.out.println(v);
-//		
-//		//v.add(c.getSimilarity(d));
-//		
-//		//System.out.println(v);
-//
-//		//System.out.println(c.isEquals(d));
-//		System.out.println(c.equals(d));
-//		System.out.println(cc.equals(dd));
-	
-		
-		
-		
-		
-		
 		try {
 			
-			// CREATING DESCRIPTION SET CONTEXT
+				// Creating description set context
+				// from an example file
+				// representing attributeSet descriptions
 			
-			DescriptionSetContext<AttributeDescription> context = new DescriptionSetContext<>("data/pc.txt");
+			String filename = "src/examples/descriptionSet/attributeSet.txt";
 			
-			// PRINTING OBSERVATIONS
+			DescriptionSetContext<AttributeDescription> context = new DescriptionSetContext<>(filename);
+			
+				// PRINTING OBSERVATIONS
 			
 			context.printObservations();
 			
-			// PRINTING INITIAL DESCRIPTION SETS
+				// PRINTING INITIAL DESCRIPTION SETS
 			
 			context.printInitialDescriptionSets();
 			
-			// PRINTING ALL DESCRIPTION SETS
+				// PRINTING ALL DESCRIPTION SETS
 			
 			context.printAllDescriptionSets();
 			
-			// PRINTING SPECIALISED INTENT
+				// PRINTING MORE SPECIALISED INTENT
+			
+			System.out.println();
 			for(String o : context.getObservations()){
-				System.out.println("Int("+o+") = "+context.getSpecIntent(o));
+				System.out.println("SpecInt("+o+") = "+context.getSpecIntent(o));
 			}
 			
-			// PRINTING INTENT
+				// PRINTING INTENT
+			
+			System.out.println();
 			for(String o : context.getObservations()){
 				System.out.println("Int("+o+") = "+context.getIntent(o));
 			}
 			
-			// PRINTING EXTENT
+				// PRINTING EXTENT
+			
+			System.out.println();
 			for(ComparableDescriptionSet ds : context.getAllDescriptionSets()){
 				System.out.println("Ext("+ds+") = "+ context.getExtent(ds));
 			}
 			
-			System.out.println("---------");
-			System.out.println("--------- ALL CLOSURES");
-			System.out.println("---------");
+				// COMPUTING AND PRINTING ALL CLOSURES 
+			
+			System.out.println();
+			System.out.println("--------- ALL CLOSURES ---------");
+			System.out.println();
 
 			System.out.println(context.allDescriptionClosures());
 		}	
